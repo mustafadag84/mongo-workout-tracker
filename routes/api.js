6dd5df4a484c763afa16f2e209735e111bbe700c
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-10-02 12:33:54 -0400

    api.js file updated

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,28 @@
 var router = require("express").Router();
 var workout = require("../models/workout.js");
 
+
 router.get("/api/workouts", function(req,res){
 workout.find().then(function(data){
     console.log(data)
     res.json(data)
 })
+.catch(err => {
+    res.status(400).json(err);
+  });
 
 });
 
-router.post("/api/workouts", function(req,res){
-    
-});
+
+router.post("/api/workouts", ({ body }, res) => {
+    workout.create(body)
+      .then(function(data) {
+        res.json(data);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 
 router.put("/api/workouts", function(req,res){
@@ -19,9 +30,15 @@ router.put("/api/workouts", function(req,res){
 });
 
 
-router.get("/api/workouts/range", function(req,res){
-    
-});
+router.post("/api/workouts/range", ({ body }, res) => {
+    workout.insertMany(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 
 
